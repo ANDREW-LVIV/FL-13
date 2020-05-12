17b18ca526fef9ed39c87744e2c171b82dda6ea4
Andriy Zhyshkovych <andrew_mail_box@yahoo.com>
2020-05-12 23:25:53 +0300

    updating hw 11

--- FL13_HW11/homework/app.js
+++ FL13_HW11/homework/app.js
@@ -1,53 +1,53 @@
 const data = [
+{
+  'folder': true,
+  'title': 'Pictures',
+  'children': [
   {
-    'folder': true,
-    'title': 'Pictures',
-    'children': [
-      {
-        'title': 'logo.png'
-      },
-      {
-        'folder': true,
-        'title': 'Vacations',
-        'children': [
-          {
-            'title': 'spain.jpeg'
-          }
-        ]
-      }
-    ]
+    'title': 'logo.png'
   },
   {
     'folder': true,
-    'title': 'Desktop',
+    'title': 'Vacations',
     'children': [
-      {
-        'folder': true,
-        'title': 'screenshots',
-        'children': null
-      }
+    {
+      'title': 'spain.jpeg'
+    }
     ]
-  },
-  {
-    'folder': true,
-    'title': 'Downloads',
-    'children': [
-      {
-        'folder': true,
-        'title': 'JS',
-        'children': null
-      },
-      {
-        'title': 'nvm-setup.exe'
-      },
-      {
-        'title': 'node.exe'
-      }
-    ]
-  },
-  {
-    'title': 'credentials.txt'
   }
+  ]
+},
+{
+  'folder': true,
+  'title': 'Desktop',
+  'children': [
+  {
+    'folder': true,
+    'title': 'screenshots',
+    'children': null
+  }
+  ]
+},
+{
+  'folder': true,
+  'title': 'Downloads',
+  'children': [
+  {
+    'folder': true,
+    'title': 'JS',
+    'children': null
+  },
+  {
+    'title': 'nvm-setup.exe'
+  },
+  {
+    'title': 'node.exe'
+  }
+  ]
+},
+{
+  'title': 'credentials.txt'
+}
 ];
 
 const rootNode = document.getElementById('root');
@@ -84,6 +84,44 @@ function tree(data, node) {
       itemIco.append('insert_drive_file');
       item.classList.add('file');
     }
+
+    // right click
+    let menu = null;
+    document.addEventListener('DOMContentLoaded', function(){
+      menu = document.querySelector('.menu');
+      menu.classList.add('off');
+      item.addEventListener('contextmenu', showmenu);
+      menu.addEventListener('mouseleave', hidemenu);
+      addMenuListeners();
+    });
+    function addMenuListeners(){
+      document.getElementById('rename').addEventListener('click', itemRename);
+      document.getElementById('delete').addEventListener('click', itemDelete);
+    }
+    function itemRename(){
+      console.log('rename clicked');
+      hidemenu();
+    }   
+    function itemDelete(){
+      console.log('delete clicked');
+      hidemenu();
+    }    
+    function showmenu(ev){
+      ev.preventDefault(); 
+      const TWENTY = 20;
+      console.log( ev.clientX, ev.clientY );
+      menu.style.top = `${ev.clientY - TWENTY}px`;
+      menu.style.left = `${ev.clientX - TWENTY}px`;
+      menu.classList.remove('off');
+    }
+    
+    function hidemenu(){
+      const TWO_HUNDRED = '-200%';
+      menu.classList.add('off');
+      menu.style.top = TWO_HUNDRED;
+      menu.style.left = TWO_HUNDRED;
+    }
+
     item.append(itemIco);
     item.append(element.title); 
     child.append(item); 
@@ -103,9 +141,21 @@ function tree(data, node) {
 
   });
 
+
   node.append(parent);
-  node.firstElementChild.classList.toggle('open');  
+  node.firstElementChild.classList.toggle('open'); 
 
 }
 
 tree(data, rootNode);
+
+function insertAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+const contextdiv = document.createElement('div');
+contextdiv.classList.add('context-menu');
+contextdiv.innerHTML = `<ul class="menu">
+<li class="menu-item" id="rename">Remane</li>
+<li class="menu-item" id="delete">Delete Item</li>
+</ul>`;
+insertAfter(rootNode, contextdiv);
